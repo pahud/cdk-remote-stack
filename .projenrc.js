21c4e1d8c8a29542438001c7ef3224c9f82e9609
Pahud <pahudnet@gmail.com>
2020-10-05 10:01:27 +0800

    chore(dev): upgrade projen and yarn

--- .projenrc.js
+++ .projenrc.js
@@ -1,17 +1,21 @@
 const {
-  ConstructLibraryAws,
+  AwsCdkConstructLibrary,
+  GithubWorkflow,
 } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.62.0';
 const PROJECT_NAME = 'cdk-remote-stack';
 const PROJECT_DESCRIPTION = 'Get outputs from cross-regional AWS CDK stacks';
+const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
-const project = new ConstructLibraryAws({
+
+const project = new AwsCdkConstructLibrary({
   authorName: "Pahud Hsieh",
   authorEmail: "hunhsieh@amazon.com",
   name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
   repository: "https://github.com/pahud/cdk-remote-stack.git",
+  antitamper: false,
   keywords: [
     'aws',
     'remote',
@@ -26,7 +30,7 @@ const project = new ConstructLibraryAws({
   },
 
   // creates PRs for projen upgrades
-  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  // projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 
   cdkVersion: AWS_CDK_LATEST_RELEASE,
   cdkDependencies: [
@@ -42,6 +46,50 @@ const project = new ConstructLibraryAws({
     distName: 'cdk-remote-stack',
     module: 'cdk_remote_stack'
   }
+});
+
+
+
+// create a custom projen and yarn upgrade workflow
+const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '0 6 * * *'
+  }], // 6am every day
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      ...project.workflowBootstrapSteps,
+
+      // yarn upgrade
+      {
+        run: `yarn upgrade`
+      },
+
+      // upgrade projen
+      {
+        run: `yarn projen:upgrade`
+      },
+
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + '}}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+        }
+      },
+    ],
+  },
 });
 
 project.mergify.addRule({
