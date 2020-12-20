a32d4610806aff32e51e4c1c9274ce1b09b44d46
Pahud <pahudnet@gmail.com>
2020-12-20 13:43:11 +0800

    update

--- .projenrc.js
+++ .projenrc.js
@@ -6,23 +6,23 @@ const PROJECT_DESCRIPTION = 'Get outputs from cross-regional AWS CDK stacks';
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
-  authorName: "Pahud Hsieh",
-  authorEmail: "hunhsieh@amazon.com",
+  authorName: 'Pahud Hsieh',
+  authorEmail: 'hunhsieh@amazon.com',
   name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
-  repository: "https://github.com/pahud/cdk-remote-stack.git",
+  repository: 'https://github.com/pahud/cdk-remote-stack.git',
   antitamper: false,
   keywords: [
     'aws',
     'remote',
     'cross-region',
     'cross-stack',
-    'cross-account'
+    'cross-account',
   ],
 
   catalog: {
     twitter: 'pahudnet',
-    announce: false
+    announce: false,
   },
 
   // creates PRs for projen upgrades
@@ -39,8 +39,8 @@ const project = new AwsCdkConstructLibrary({
 
   python: {
     distName: 'cdk-remote-stack',
-    module: 'cdk_remote_stack'
-  }
+    module: 'cdk_remote_stack',
+  },
 });
 
 // create a custom projen and yarn upgrade workflow
@@ -48,7 +48,7 @@ workflow = project.github.addWorkflow('ProjenYarnUpgrade');
 
 workflow.on({
   schedule: [{
-    cron: '0 6 * * *'
+    cron: '0 6 * * *',
   }], // 6am every day
   workflow_dispatch: {}, // allow manual triggering
 });
