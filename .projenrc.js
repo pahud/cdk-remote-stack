93a080092091d53adb96d9225958261ea29fd1d8
Pahud Hsieh <pahudnet@gmail.com>
2021-08-31 14:48:54 +0800

    fix the release workflow (#358)

--- .projenrc.js
+++ .projenrc.js
@@ -2,12 +2,12 @@ const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen
 
 const AWS_CDK_LATEST_RELEASE = '1.77.0';
 const PROJECT_NAME = 'cdk-remote-stack';
-const PROJECT_DESCRIPTION = 'Get outputs from cross-regional AWS CDK stacks';
+const PROJECT_DESCRIPTION = 'Get outputs and parameters from remote CDK stacks';
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
   authorName: 'Pahud Hsieh',
-  authorEmail: 'hunhsieh@amazon.com',
+  authorEmail: 'pahudnet@gmail.com',
   name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
   repository: 'https://github.com/pahud/cdk-remote-stack.git',
@@ -20,6 +20,7 @@ const project = new AwsCdkConstructLibrary({
     'cross-account',
   ],
   depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
       secret: AUTOMATION_TOKEN,
@@ -29,7 +30,7 @@ const project = new AwsCdkConstructLibrary({
     secret: 'GITHUB_TOKEN',
     allowedUsernames: ['pahud', 'cdk-automation'],
   },
-  defaultReleaseBranch: 'master',
+  defaultReleaseBranch: 'main',
   catalog: {
     twitter: 'pahudnet',
     announce: false,
